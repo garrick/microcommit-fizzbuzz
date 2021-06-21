d467269a794c05eea8f2637dc4f8c791ac47a457
Garrick West <garrick@acm.org>
2021-06-21 16:52:31 -0700

    Simplest check of 3 to return 'Fizz' and pass all tests

--- app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
+++ app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
@@ -2,6 +2,7 @@ package org.commandline.microcommitfizzbuzz;
 
 public class FizzBuzz {
     public String convert(int intValue) {
+        if(intValue == 3) return "Fizz";
         return ""+intValue;
     }
 }
