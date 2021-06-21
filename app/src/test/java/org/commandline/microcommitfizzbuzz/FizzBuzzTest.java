a15802ea04f9fa10cebb65934b662f0282ebb53b
Garrick West <garrick@acm.org>
2021-06-21 16:25:24 -0700

    Removed intentional fail and supported static import. Now passing.

--- app/src/test/java/org/commandline/microcommitfizzbuzz/FizzBuzzTest.java
+++ app/src/test/java/org/commandline/microcommitfizzbuzz/FizzBuzzTest.java
@@ -2,12 +2,9 @@ package org.commandline.microcommitfizzbuzz;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class FizzBuzzTest {
 
     @Test
     public void testClassRecognizedAndMethodFailsBuildIntentionally(){
-        fail("We should fail!");
     }
 }
