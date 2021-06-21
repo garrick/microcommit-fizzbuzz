6b8b86bbe4255c652f6c1984b2a3f48b6f2d5260
Garrick West <garrick@acm.org>
2021-06-21 16:59:33 -0700

    Modifying FizzBuzz.convert to handle multiples of 3 for Fizz

--- app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
+++ app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.commandline.microcommitfizzbuzz;
 
 public class FizzBuzz {
     public String convert(int intValue) {
-        if(intValue == 3) return "Fizz";
+        if (intValue % 3 == 0) return "Fizz";
         return ""+intValue;
     }
 }
