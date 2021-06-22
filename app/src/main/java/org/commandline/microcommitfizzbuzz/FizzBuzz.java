60863f218cb2d015881f976b11a05323d4ffe9df
Garrick West <garrick@acm.org>
2021-06-21 17:00:13 -0700

    Fixing spacing formatting on return line for convert

--- app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
+++ app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
@@ -3,6 +3,6 @@ package org.commandline.microcommitfizzbuzz;
 public class FizzBuzz {
     public String convert(int intValue) {
         if (intValue % 3 == 0) return "Fizz";
-        return ""+intValue;
+        return "" + intValue;
     }
 }
