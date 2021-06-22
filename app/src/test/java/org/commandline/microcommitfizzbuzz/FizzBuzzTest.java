efbbe5f9f1503eb134dfdc3f2b6e975db9c125c7
Garrick West <garrick@acm.org>
2021-06-21 17:15:34 -0700

    Updated 'FizzBuzzTest' to Test for multiples of 3 and 5 on convert

--- app/src/test/java/org/commandline/microcommitfizzbuzz/FizzBuzzTest.java
+++ app/src/test/java/org/commandline/microcommitfizzbuzz/FizzBuzzTest.java
@@ -49,7 +49,7 @@ public class FizzBuzzTest {
     @Test
     public void testConvertReturnsStringFizzBuzzForFifteen(){
         //Act
-        String result = unit.convert(15);
+        String result = unit.convert(3 * 5 * 2);
         //Assert
         assertEquals("FizzBuzz", result);
     }
