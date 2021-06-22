b091b46baa27eab5a2ce1f4396f37b4a97d51009
Garrick West <garrick@acm.org>
2021-06-21 17:05:42 -0700

    Adding minimum implementation to FizzBuzz.convert to pass for 5 to 'Buzz'

--- app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
+++ app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
@@ -2,6 +2,7 @@ package org.commandline.microcommitfizzbuzz;
 
 public class FizzBuzz {
     public String convert(int intValue) {
+        if (intValue == 5) return "Buzz";
         if (intValue % 3 == 0) return "Fizz";
         return "" + intValue;
     }
