81cd3caa80d5664aa260e92a85be455b6fcacdc5
Garrick West <garrick@acm.org>
2021-06-21 17:08:59 -0700

    Updated FizzBuzz to return 'Buzz' for multiples of 5

--- app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
+++ app/src/main/java/org/commandline/microcommitfizzbuzz/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.commandline.microcommitfizzbuzz;
 
 public class FizzBuzz {
     public String convert(int intValue) {
-        if (intValue == 5) return "Buzz";
+        if (intValue % 5 == 0) return "Buzz";
         if (intValue % 3 == 0) return "Fizz";
         return "" + intValue;
     }
